444e7a4622eacb5589b32d38ffdde970e3865820
Pandong <jandranrebbalangue@gmail.com>
2021-01-21 23:59:03 +0800

    change data type for price

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -2,16 +2,20 @@ class Product {
   String id;
   String name;
   String category;
-  int price;
+  String price;
   String bgColor;
-  Product({this.id, this.name, this.category, this.price, this.bgColor});
-
-  factory Product.fromjson(Map<String, dynamic> json) {
-    return Product(
-        id: json['id'],
-        name: json['name'],
-        category: json['category'],
-        price: json['price'],
-        bgColor: json['bgColor']);
-  }
+  Product({
+    this.id,
+    this.name,
+    this.category,
+    this.price,
+    this.bgColor,
+  });
+  factory Product.fromJson(Map<String, dynamic> json) => Product(
+        id: json["id"],
+        name: json["name"],
+        category: json["category"],
+        price: json["price"],
+        bgColor: json["bgColor"],
+      );
 }
